bfad1513bd9f8fe9d6866d341492f5fea1169979
Saurav Chatterjee <saurav.chatterjee@asurion.com>
2019-01-02 13:35:56 -0800

    pass in cycle counter when drawing dinosaur

--- Dinosaur.js
+++ Dinosaur.js
@@ -14,7 +14,7 @@ var down = false
 var height = 0
 var difT = 0
 var tNum = 0
-var tNum2 = 0; 
+var tNum2 = 0;
 var dinoSizeY = 100
 var dinoSizeX = 50
 
@@ -22,83 +22,83 @@ var dinoSizeX = 50
 var dino_x = 50;
 var dino_y = 48 * 10;
 var dino1 = [
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,
-0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,
-0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,
-1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,
-1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,
-1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,
-1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,
-1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,
-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
-0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
-0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,
-0,0,0,0,0,1,1,1,0,1,1,1,0,0,0,0,0,0,0,
-0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,
-0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,
-0,0,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,0,0];
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0];
 
 const dino2 = [
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,
-    0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,
-    0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,
-    1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,
-    1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,
-    1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,
-    1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,
-    1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-    0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-    0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
-    0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
-    0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,
-    0,0,0,0,0,1,1,1,0,0,0,1,0,0,0,0,0,0,0,
-    0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0,0,0,0,
-    0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0];
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 
 const dino3 = [
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,
-      0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-      0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,
-      0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-      0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,
-      1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-      1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,
-      1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,
-      1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,
-      1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,
-      1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,
-      1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-      0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-      0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,
-      0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
-      0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
-      0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,
-      0,0,0,0,0,1,0,0,0,1,1,1,0,0,0,0,0,0,0,
-      0,0,0,0,0,1,1,1,0,0,1,1,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0];
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0];
 window.addEventListener(
   'keydown',
   function (event) {
@@ -122,7 +122,7 @@ window.addEventListener(
 var game = init();
 
 
-function init () {
+function init() {
   let game = new Array(SIZEX)
   for (var i = 0; i < SIZEX; i++) {
     game[i] = new Array(SIZEY)
@@ -134,14 +134,14 @@ function init () {
 
   game[100][10] = 1
 
-  let bitValue = getDinoBitmapValue (0, 2, 1);
+  let bitValue = getDinoBitmapValue(0, 2, 1);
   console.log('bitValue = ' + bitValue);
 
   console.log('init done')
   return game
 }
 
-function initDino () {
+function initDino() {
   let game = new Array(200)
   for (var i = 0; i < SIZEX; i++) {
     game[i] = new Array(SIZEY)
@@ -166,11 +166,11 @@ var intervalVar = setInterval(player, interval)
 // setInterval(cacti, 10000000);
 cacti(10)
 
-function player () {
+function player() {
   play()
 }
 
-function play () {
+function play() {
   console.log('play() called')
   moveRoad()
   drawGame()
@@ -178,16 +178,16 @@ function play () {
   // dinosaur part
 
   if (up == true) {
-    dino_y -= differenceFinder(tNum,tNum2)
+    dino_y -= differenceFinder(tNum, tNum2)
     tNum++
   }
   if (down == true) {
-        
-            dino_y += differenceFinder(tNum,tNum2)
-            tNum+=0.000000001;
-            tNum2+=0.000000001;
-        
-   
+
+    dino_y += differenceFinder(tNum, tNum2)
+    tNum += 0.000000001;
+    tNum2 += 0.000000001;
+
+
   }
 
   if (dino_y >= 490) {
@@ -195,19 +195,19 @@ function play () {
     down = false
   }
 
-  
-  if(dino_y > SIZEY/2){
-        down = false;
-        up = false;
-        tNum = 0; 
-        tNum2=-1;
+
+  if (dino_y > SIZEY / 2) {
+    down = false;
+    up = false;
+    tNum = 0;
+    tNum2 = -1;
   }
   if (dino_y <= 300) {
-      down = true
-      up = false
-      tNum = 0;
-      tNum2=-1;
-    }
+    down = true
+    up = false
+    tNum = 0;
+    tNum2 = -1;
+  }
   //console.log("dino_y = " + dino_y);
   /*if(dino_y/10 <= SIZEY/2){
         down = false;
@@ -215,7 +215,7 @@ function play () {
 
   }*/
   // displayDino(height)
-  drawDino();
+  drawDino(0);
   collision(dino_y, dino_x)
   // cactus
   heartBeat++
@@ -241,14 +241,15 @@ function play () {
   }
 }
 
-function cacti () {
+function cacti() {
   makeCactus(100)
 }
-function bigcacti () {
+
+function bigcacti() {
   makeCactusBig(100)
 }
 
-function test () {
+function test() {
   let count = 0
   for (k = 0; k < 100; k++) {
     clear()
@@ -264,11 +265,11 @@ function test () {
   }
 }
 
-async function pause () {
+async function pause() {
   await sleep(1)
 }
 
-function createRoad () {
+function createRoad() {
   var pixel = 0
   var height = SIZEY / 2
   while (pixel < SIZEX) {
@@ -277,13 +278,13 @@ function createRoad () {
   }
 }
 
-function clear () {
+function clear() {
   ctx.fillStyle = 'white'
   ctx.fillRect(0, 0, SIZEX * 10, SIZEY * 10)
   ctx.fillStyle = 'black'
 }
 
-function moveRoad () {
+function moveRoad() {
   // console.log("moveRoad() called");
 
   // for(let y = 0; y < SIZEY; y++){
@@ -303,7 +304,7 @@ function moveRoad () {
   }
 }
 
-function drawGame () {
+function drawGame() {
   // console.log('drawGame() called');
   for (let i = 0; i < SIZEX; i++) {
     for (let v = 0; v < SIZEY; v++) {
@@ -319,7 +320,7 @@ function drawGame () {
   }
 }
 
-function makeCactus (xCoordinate) {
+function makeCactus(xCoordinate) {
   console.log('makeCactus() called')
 
   for (let x = 0; x < 2; x++) {
@@ -332,7 +333,8 @@ function makeCactus (xCoordinate) {
     game[xCoordinate + x + 1][SIZEY / 2 - 5] = 1
   }
 }
-function makeCactusBig (xCoordinate) {
+
+function makeCactusBig(xCoordinate) {
   console.log('makeCactusBig() called')
   // first cactus
   for (let x = 0; x < 2; x++) {
@@ -383,7 +385,7 @@ function makeCactusBig (xCoordinate) {
  * draws the dinosaur
  *
  */
-function displayDinosaur () {
+function displayDinosaur() {
   console.log('displayWorld() called')
 
   /*
@@ -407,22 +409,26 @@ function displayDinosaur () {
   ctx.fillRect(dino_x, dino_y - dinoSizeY, dinoSizeX, dinoSizeY)
 }
 
-function dino_up () {
+function dino_up() {
   console.log('dino_up(): called')
   up = true
   tNum = 1
 }
-function dino_down () {}
+
+function dino_down() {
+}
+
 /**
  * finds the differnce in heights and how much higher the dino should move
  * @param {} t
  */
-function differenceFinder (t, t0) {
+function differenceFinder(t, t0) {
   let height2 = STARTINGVELOVITY * t + -0.0009 * (t * t) / 2
   let height1 = STARTINGVELOVITY * (t0) + -0.0009 * ((t0) * (t0)) / 2
   return height2 - height1
 }
-function collision (yVal, xVal) {
+
+function collision(yVal, xVal) {
   let scaleY = Math.round(yVal / 10.0)
   let scaleX = Math.round(xVal / 10.0)
   let hit = false
@@ -437,36 +443,35 @@ function collision (yVal, xVal) {
   console.log('collision(): hit=' + hit)
   return hit
 }
-function drawDino(){
+
+function drawDino(cycle) {
 //goes through every single value in the step
-      for(var x = 0; x < 18; x ++){
-            for(var y = 0; y < 25; y ++){
-                  // checks if that value is filled in
-                  if(getDinoBitmapValue(0,x,y) == 1){
-                    console.log("drawDino() - got 1 at x=" + x + ", y=" + y);
-                    ctx.fillStyle = 'red';
-                    ctx.fillRect(x*4,y*4+SIZEY/2,4,4)
-                  }
-
-            }
+  for (var x = 0; x < 18; x++) {
+    for (var y = 0; y < 25; y++) {
+      // checks if that value is filled in
+      if (getDinoBitmapValue(cycle, x, y) == 1) {
+        console.log("drawDino() - got 1 at x=" + x + ", y=" + y);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(x * 4, y * 4 + SIZEY / 2, 4, 4)
       }
-} 
+
+    }
+  }
+}
 
 
-
-    /**
-     * Return whether the dinosaur has a 0 or 1 at this x, y location for the given counter (0, 1, 2)
-     * @param {*} cycleCounter 
-     * @param {*} x 
-     * @param {*} y 
-     */
-function getDinoBitmapValue (cycleCounter, x, y)
-{
+/**
+ * Return whether the dinosaur has a 0 or 1 at this x, y location for the given counter (0, 1, 2)
+ * @param {*} cycleCounter value between 0 and 2
+ * @param {*} x
+ * @param {*} y
+ */
+function getDinoBitmapValue(cycleCounter, x, y) {
 // get the right variable based on the counter
-let dino = (cycleCounter == 0) ? dino1 : dino2;
+  let dino = (cycleCounter == 0) ? dino1 : ((cycleCounter == 1) ? dino2 : dino3);
 
-let index = y * 19 + x;
-return dino[index];
+  let index = y * 19 + x;
+  return dino[index];
 }
 
 
